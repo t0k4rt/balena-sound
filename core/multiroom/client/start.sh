f89e0763c45e3f1ef939268eb15e67aea170c248
Alexandre Assouad <assouad@kairosfire.com>
2020-11-15 11:10:18 +0100

    removed supervisor part, snapserver must be set using env vars

--- core/multiroom/client/start.sh
+++ core/multiroom/client/start.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Wait for sound supervisor to start
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):3000"
-while ! curl --silent --output /dev/null "$SOUND_SUPERVISOR/ping"; do sleep 5; echo "Waiting for sound supervisor to start"; done
-
-# Get mode and snapserver from sound supervisor
-# mode: default to MULTI_ROOM
-# snapserver: default to multiroom-server (local)
-MODE=$(curl --silent "$SOUND_SUPERVISOR/mode" || true)
-SNAPSERVER=$(curl --silent "$SOUND_SUPERVISOR/multiroom/master" || true)
-
 # --- ENV VARS ---
 # SOUND_MULTIROOM_LATENCY: latency in milliseconds to compensate for speaker hardware sync issues
 LATENCY=${SOUND_MULTIROOM_LATENCY:+"--latency $SOUND_MULTIROOM_LATENCY"}
