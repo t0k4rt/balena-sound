9cc2ece4d54a8bdd89ae188857afca005739f816
Alexandre Assouad <assouad@kairosfire.com>
2020-11-15 11:26:30 +0100

    force mode to multi room client

--- core/audio/start.sh
+++ core/audio/start.sh
@@ -63,15 +63,6 @@ function reset_sound_config() {
   cp "$CONFIG_TEMPLATE" "$CONFIG_FILE"
 }
 
-# Wait for sound supervisor to start
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):3000"
-while ! curl --silent --output /dev/null "$SOUND_SUPERVISOR/ping"; do sleep 5; echo "Waiting for sound supervisor to start"; done
-
-# Get mode from sound supervisor. 
-# mode: default to MULTI_ROOM
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):3000"
-MODE=$(curl --silent "$SOUND_SUPERVISOR/mode" || true)
-
 # Audio routing: route intermediate balena-sound input/output sinks
 echo "Setting audio routing rules. Note that this can be changed after startup."
 reset_sound_config
